69a27775405ea4c944aa632b44e874da53349ab0
colewortman <wortcole12@gmail.com>
2024-11-26 02:42:07 -0600

    Added movie card component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import '../styles.css';
+import React from "react";
+import "../styles.css";
 
-export default function Footer(){
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
 
-    const currentYear = new Date().getFullYear();
-
-    return (
-        <footer className='footer'>
-            <p className='footer-text'>
-            © {currentYear} Moviedux, All rights reserved.
-            </p>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        © {currentYear} Moviedux, All rights reserved.
+      </p>
+    </footer>
+  );
 }
